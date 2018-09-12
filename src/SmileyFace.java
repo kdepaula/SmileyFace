eee7a4aa15e2d8d16a3ce15594361f2a16420169
kdepaula <42978472+kdepaula@users.noreply.github.com>
2018-09-12 14:07:46 -0400

    Add files via upload

---
+++ src/SmileyFace.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @author kdepaula20
+ * smiley face
+ * Period 7
+ *
+ */
+public class SmileyFace {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("\\\\\\\\\\\\\\\\\\");
+		System.out.println("| *   * |");
+		System.out.println("|   *   |");
+		System.out.println("| \\__/  |");
+		System.out.println("\\_______/");
+	}
+
+}
